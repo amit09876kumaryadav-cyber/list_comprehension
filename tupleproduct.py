c93a5bb6552146ca70c5b70c0ad9a316b560254b
amit09876kumaryadav-cyber <amit09876kumaryadav@gmail.com>
2025-08-27 14:56:01 +0530

    Add files via upload

---
+++ tupleproduct.py
@@ -0,0 +1,9 @@
+#write a program to calculate the product,multiplying all the numbers of the given tuple
+#step 1: Define a tuple with numbers
+numbers = (1, 2, 3, 4, 5)
+
+    #step 3: Use a loop to multiply numbers one by one
+for num in numbers:
+        product *= num
+#step 4: Show the product
+print("The product of numbers in the tuple is:", product)
